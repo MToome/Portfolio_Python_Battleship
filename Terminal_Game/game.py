cdc6ca79d518a7c2266a6179c2bb39f265c9ff75
MToome <128951746+MToome@users.noreply.github.com>
2023-05-25 08:29:34 +0300

    Initial data

--- Terminal_Game/game.py
+++ Terminal_Game/game.py
@@ -1,1 +1,10 @@
 #Python Terminal Game
+
+player1 = input("Player 1 enter your name: ")
+player2 = input("Player 2 enter your name: ")
+
+carrier = 5
+battleship = 4
+destroyer = 3
+submarine = 3
+patrol_boat = 2
