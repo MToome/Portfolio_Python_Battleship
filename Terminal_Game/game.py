831588c4abd3f27185f5bbc69e0201b2b63a670d
MToome <128951746+MToome@users.noreply.github.com>
2023-05-28 11:29:59 +0300

    Can change the size of the board

--- Terminal_Game/game.py
+++ Terminal_Game/game.py
@@ -3,24 +3,26 @@
 #player1 = input("Player 1 enter your name: ")
 #player2 = input("Player 2 enter your name: ")
 
+#Ship names
 carrier = 5
 battleship = 4
 destroyer = 3
 submarine = 3
 patrol_boat = 2
 
-board = ["""
- A   B   C   D   E   F
-"""]
+#Board
+alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-
-for i in range(6):
-    i += 1
-    board.append(str(i) +("[ ] "*6))
-
-
-def show_board(board):
+def show_board(size):
+    header = []
+    board  = [header]
+    for i in range(size):
+        header.append("    " + alphabet[i])
+    for i in range(size):
+        i += 1
+        board.append(str(i) +("[ ]"*size))
     for row in board:
         print((" ").join(row))
 
-show_board(board)
+show_board(9)
+
