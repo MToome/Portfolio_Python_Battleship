10402041f6e6248926fb0a5fed0d4bfffff5a826
MToome <128951746+MToome@users.noreply.github.com>
2023-05-29 12:39:12 +0300

    Added guessing

--- Terminal_Game/game.py
+++ Terminal_Game/game.py
@@ -1,3 +1,5 @@
+import random
+
 #Python Terminal Game
 
 #player1 = input("Player 1 enter your name: ")
@@ -22,9 +24,12 @@ def show_board(size):
     for row in board:
         print((" ").join(row))
 
-show_board(9)
+#Testing Board function
+#show_board(9)
 
 #Ship spawn
+"""
+For multiplayer
 def spawn_ship(size, orientation, row, col):
     ship_len = size
     ship_orientation = orientation
@@ -38,7 +43,31 @@ def spawn_ship(size, orientation, row, col):
         ship_row = (row-1)
         row_range = list(range(ship_row,ship_row + ship_len))
         cor = tuple(zip(ship_col, row_range))
-    print(list(cor))
+    return list(cor)
+"""
+def spawn_ship(size):
+    ship_len = random.randint(1,size)
+    ship_orientation = random.randint(0,1)
+    if ship_orientation == 0:
+        ship_row = [random.randint(0, size-1)]*size
+        ship_col = random.randint(0, size - ship_len)
+        col_range = list(range(ship_col,ship_col + ship_len))
+        cor = tuple(zip(ship_row, col_range))
+    else:
+        ship_col = [random.randint(0, size-1)]*size
+        ship_row = random.randint(0, size - ship_len)
+        row_range = list(range(ship_row,ship_row + ship_len))
+        cor = tuple(zip(ship_col, row_range))
+    return list(cor)
 
-ship = spawn_ship(3, 1, 2, 4); ship
+
+#Testing Ship spawning function 
+#cruiser = spawn_ship(3)
+
+#Guessing
+def player_guess():    
+    guess_col = int(input("Col: ")) - 1
+    guess_row = int(input("Row:")) - 1
+    return guess_col, guess_row
+
 
