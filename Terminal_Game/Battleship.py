cbad1b95524a6b49f75e9f3a64879c14ef95acac
MToome <128951746+MToome@users.noreply.github.com>
2023-06-06 05:47:04 +0300

    Little changes

--- Terminal_Game/Battleship.py
+++ Terminal_Game/Battleship.py
@@ -2,30 +2,8 @@ import random
 
 #Python Terminal Game
 
-#player1 = input("Player 1 enter your name: ")
-#player2 = input("Player 2 enter your name: ")
-
-"""
-Ship names
-carrier = 5
-battleship = 4
-destroyer = 3
-submarine = 3
-patrol_boat = 2
-"""
-
 #Board
-"""
-def show_board(size):
-    header = []
-    board  = [header]
-    for i in range(size):
-        header.append("    " + str(i+1))
-    for i in range(size):
-        board.append(str(i+1) +("[ ]"*size))
-    for row in board:
-        print((" ").join(row))
-"""
+
 def build_board(dims):
     return [['O' for _ in range(dims)] for _ in range(dims)]
 
@@ -34,23 +12,7 @@ def print_board(board):
         print(*row)
 
 #Ship spawn
-"""
-For multiplayer
-def spawn_ship(size, orientation, row, col):
-    ship_len = size
-    ship_orientation = orientation
-    if ship_orientation == 0:
-        ship_row = [row-1]*size
-        ship_col = (col-1)
-        col_range = list(range(ship_col,ship_col + ship_len))
-        cor = tuple(zip(ship_row, col_range))
-    else:
-        ship_col = [col-1]*size
-        ship_row = (row-1)
-        row_range = list(range(ship_row,ship_row + ship_len))
-        cor = tuple(zip(ship_col, row_range))
-    return list(cor)
-"""
+
 def spawn_ship(dims):
     ship_len = random.randint(1,dims)
     ship_orientation = random.randint(0,1)
