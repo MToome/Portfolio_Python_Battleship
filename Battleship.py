ae0aa2fd3ce5caed596a9d369c0a6a739f78ccc4
MToome <128951746+MToome@users.noreply.github.com>
2023-12-18 05:57:36 +0200

    Spellcheck, MToome

--- Battleship.py
+++ Battleship.py
@@ -14,53 +14,53 @@ def print_board(board):
         print(*row)
 
 # Ship spawn
-# Randomly generates sthe ship and randomly also chooses the orientation and location
+# Randomly generates the ship and randomly also chooses the orientation and location
 def spawn_ship(dims):
-    # Ship lenght is randomly selected between 1 and dims size
+    # Ship length is randomly selected between 1 and dims size
     ship_len = random.randint(1,dims)
     # Ship orientation is randomly selected between 0 and 1
     ship_orientation = random.randint(0,1)
     # Checks if the orientation is 0 then it generates the random location
     if ship_orientation == 0:
-        # Selects the row for the ship randomly between 0 and inputed dims size - 1, then multyples it to ship length list
+        # Selects the row for the ship randomly between 0 and input dims size - 1, then multiples it to the ship length list
         ship_row = [random.randint(0, dims-1)] * ship_len
-        # Pick the first cordinate for the ship, have to subtrack the ship lenght to make sure the ship would not go off board
+        # Pick the first coordinate for the ship, have to subtract the ship length to make sure the ship would not go off board
         ship_col = random.randint(0, dims - ship_len)
-        # Zip combines the ship_row and ship cord, list makes it to list 
+        # Zip combines the ship_row and ship cord, and the list makes it to the list 
         cor = list(zip(ship_row, range(ship_col, ship_col + ship_len)))
     # If the orientation is not 0 it will do the following
     else:
-        # Selects the row for the ship randomly between 0 and inputed dims size - 1, then multyples it to ship length list
+        # Selects the row for the ship randomly between 0 and input dims size - 1, then multiples it to the ship length list
         ship_col = [random.randint(0, dims-1)]* ship_len
-        # Pick the first cordinate for the ship, have to subtrack the ship lenght to make sure the ship would not go off board
+        # Pick the first coordinate for the ship, have to subtract the ship length to make sure the ship would not go off board
         ship_row = random.randint(0, dims - ship_len)
-        # Zip combines the ship_row and ship cord, list makes it to list
+        # Zip combines the ship_row and ship cord, and the list makes it to the list
         cor = list(zip(range(ship_row, ship_row + ship_len), ship_col))
-    # Returns the cordinates
+    # Returns the coordinates
     return cor
 
 #Guessing
-#This is for guessing where the ship is, also their is exit function in it and breaking protection from entering letters or to high or low number.
+#This is for guessing where the ship is, also there is an exit function in it and breaking protection from entering letters or to high or low numbers.
 def player_shoot():    
     # As long as the value is True this loop will run
     while True:
         # User input is asked
         shoot_col = input("Col: ") 
-        # If user input is "exit" it will end the game, .lower() is fail safe for if user has caps lock on
+        # If the user input is "exit" it will end the game, .lower() is fail-safe if the user has caps lock on
         if shoot_col.lower() == "exit" or shoot_col.lower() == "q":
-            # Built in exit function
+            # Built-in exit function
             exit()
-        # This try helps to prevent game blowing up
+        # This try helps to prevent the game from blowing up
         try:
-            # Turns user input into integer
+            # Turns user input into an integer
             int(shoot_col)
         # If try fails does this
         except:
             # Shows this
-            print("Please enter a number without a decimel")
+            print("Please enter a number without a decimal")
             # restarts the loop
             continue
-        # When input is higher the board size or lower then 1 
+        # When the input is higher than the board size or lower than 1 
         if int(shoot_col) > len(board_size) or int(shoot_col) < 1:
             # It shows this
             print("Please, enter number in the board.")
@@ -78,14 +78,14 @@ def player_shoot():
         try:
             int(shoot_row)
         except:
-            print("Please enter a number without a decimel")
+            print("Please enter a number without a decimal")
             continue
         if int(shoot_row) > len(board_size) or int(shoot_row) < 1:
             print("Please, enter number in the board.")
             continue
         else:
             break
-    # This ends the function and returns the input reduced by one because python starts counting at 0
+    # This ends the function and returns the input reduced by one because Python starts counting at 0
     return (int(shoot_row)- 1, int(shoot_col) - 1)
 
 
@@ -93,11 +93,11 @@ def player_shoot():
 #Updating
 #Changing the board, showing where have been shot and where the ship has been hit.
 def update(guess, board, ship, guess_list):
-    # Checks if the guess player made is allready made and if it is
+    # Checks if the guess the player made is already made and if it is
     if guess in guess_list:
         # it shows this text
-        print("You allready shot their")
-    # When player hits the ship
+        print("You already shot their")
+    # When the player hits the ship
     elif guess in ship:
         # Shows this text
         print("Direct hit")
@@ -113,7 +113,7 @@ def update(guess, board, ship, guess_list):
         board[guess[0]][guess[1]] = "-"
         # Enters the guess to the guess_list
     guess_list.append(guess)
-    # Ends the functin and returns the updated board
+    # Ends the function and returns the updated board
     return board
 
 #Defining the board size
@@ -135,27 +135,27 @@ def main():
         print_board(board_size)
         # Generats the ship
         cruiser = spawn_ship(len(board_size))  
-        # Empty list where values are store after guessing
+        # Empty list where values are stored after guessing
         guess_list = []
-        # Game end is defined, if the ship lenght is 0 game ends, until then it cycles
+        # Game end is defined, if the ship length is 0 game ends, until then it cycles
         while len(cruiser) > 0:
             # Calls the guessing function
             player_guess = player_shoot()
-            # Changes the board and checking the ship location
+            # Changes the board and check the ship location
             b = update(player_guess, b, cruiser, guess_list)
             # Shows the board
             print_board(board_size)
-        # Prints after cycle has ended
+        # Prints after the cycle has ended
         print("Congrats you sunk the ship")
         # Asks the player input if the player wants  to play again
         game_restart = input("Do you want to play again? y or n : ")
-        # .lower() makes the ansewr lower case and the it checks if it is y
+        # .lower() makes the answer lower case and it checks if it is y
         if game_restart.lower() == "y":
-            # If it is y loop will run as many times as their was guesses
+            # If it is y loop will run as many times as there were guesses
             for i in range(len(guess_list)):
-                # Num gets the valu of one guess, row and column
+                # Num gets the value of one guess, row, and column
                 num = guess_list[i]
-                # This changes all the hits and miss markers to O, their would be a clean board again
+                # This changes all the hits and miss markers to O, there would be a clean board again
                 board_size[num[0]][num[1]] = "O"
             # Emptys the guess list
             guess_list.clear()
